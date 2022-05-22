52f0ce7ba13627955ff36ab6ac8375f404d9fe9b
André Vinicius Bezerra da Silva <105171947+AVBDS001@users.noreply.github.com>
2022-05-22 15:23:34 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds  = document.getElementById("seconds");
+
+const currentYear = new Date().getFullYear();
+const newYearTime = new Date(`January 1 ${currentYear + 1} 00:00:00`);
+
+//Update countdown time
+function updateCountdownTime() {
+    const currentTime = new Date();
+    const diff = newYearTime - currentTime;
+    const d = Math.floor(diff / 1000 / 86400);
+    const h = Math.floor(diff / 1000 / 3600) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' : h;
+    minutes.innerHTML = m < 10 ? '0' : m;
+    seconds.innerHTML = s < 10 ? '0' : s;
+
+}
+
+setInterval(updateCountdownTime, 1000);
+
